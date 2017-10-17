e2440f73a831e08310a53be8bb576e10a1d27ec3
Joy <joyyeh.tw@gmail.com>
2017-10-18 07:12:57 +0800

    Integrated JSONForm, but new version of bootstrap has incomhas incompatibility; in process of fixing

--- Cenarius/Controllers/HomeController.cs
+++ Cenarius/Controllers/HomeController.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Mvc.Ajax;
+using System.Web.Script.Serialization;
 
 namespace Cenarius.Controllers
 {
@@ -16,6 +18,23 @@ namespace Cenarius.Controllers
 
             ViewData["Version"] = mvcName.Version.Major + "." + mvcName.Version.Minor;
             ViewData["Runtime"] = isMono ? "Mono" : ".NET";
+
+            ViewData["Title"] = "新光醫院 [乳房超音波診斷] 資料輸入頁面";
+            ViewData["Heading"] = "乳房超音波 :: 診斷報告";
+            ViewData["Subheading"] = "【附表二】乳房超音波檢查報告單";
+
+            string bcuPathHeader = "~/FormSchema/BreastCancerUltrasound";
+            string schemaPath = HttpContext.Server.MapPath(bcuPathHeader + "Schema.json");
+            string formPath = HttpContext.Server.MapPath(bcuPathHeader + "Form.json");
+            string paramsPath = HttpContext.Server.MapPath(bcuPathHeader + "Params.json");
+
+            string schemaStr = System.IO.File.ReadAllText(schemaPath);
+            string formStr = System.IO.File.ReadAllText(formPath);
+            string paramsStr = System.IO.File.ReadAllText(paramsPath);
+
+            ViewData["Schema"] = schemaStr;
+            ViewData["Form"] = formStr;
+            ViewData["Params"] = paramsStr;
 
             return View();
         }
